9c411dc3cb6dfe6fc38f7899a9ae46a297a4c64c
Zack Klinger <zklinger@gmail.com>
2016-10-18 08:59:17 -0700

    CREATE a momentjs based timestamp service and landing page

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var moment = require('moment');
 var express = require('express');
 var app = express();
 
@@ -13,8 +14,47 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/*', function(req, res) {
+  var param = req.params[0];
+  var dateArray, month, day, year, stringDate, numDate;
+
+  numDate = Number(param);
+
+  if (numDate && moment.unix(numDate).isValid()) {
+    return res.json({
+      "unix": numDate,
+      "natural": moment.unix(numDate).format('MMMM D, YYYY')
+    });
+  } else {
+    dateArray = param.split(' ');
+
+    if (dateArray.length === 3) {
+      month = dateArray[0];
+      day = dateArray[1].slice(0, 2);
+      year = dateArray[2];
+
+      stringDate = moment(month + ' ' + day + ', ' + year, 'MMMM D, YYYY');
+
+      if (stringDate.isValid()) {
+        res.json({
+          "unix": stringDate.unix(),
+          "natural": stringDate.format('MMMM D, YYYY')
+        });
+      }
+    } else {
+      res.json({
+        "unix": null,
+        "natural": null
+      });
+    }
+  }
+
+});
+
+app.post('/api/timestamp', function(req, res) {
+  res.json({ Date: Date.now() });
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
